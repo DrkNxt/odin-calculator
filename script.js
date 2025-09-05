655cad7422e1ba84e918a33be4d9cf4f7aa3045f
DrkNxt <65965412+DrkNxt@users.noreply.github.com>
2025-09-05 17:02:37 +0200

    Add full calculator functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let firstNumber = 0;
-let secondNumber = 0;
+const DISPLAY = document.querySelector("#display");
+const BUTTONS = document.querySelector("#buttons");
+
+let firstNumber = null;
 let operator = "";
+let displayText = "0";
+let hasDecimal = false;
+let displayShowsResult = false;
+let displayShowsError = false;
+let lastButtonWasOperator = false;
 
 function add(x, y) {
     return x+y;
@@ -34,21 +41,155 @@ function mod(x, y) {
 }
 
 function operate(x, y, op) {
+    let num = y;
     switch (op) {
-        case "+":
-            add(x,y);
+        case "add":
+            num = add(x,y);
             break;
-        case "-":
-            sub(x,y);
+        case "sub":
+            num = sub(x,y);
             break;
-        case "*":
-            mul(x,y);
+        case "mul":
+            num = mul(x,y);
             break;
-        case "/":
-            div(x,y);
+        case "div":
+            num = div(x,y);
             break;
-        default:
-            return false;
+        case "mod":
+            num = mod(x,y);
+            break;
     }
-    return true;
+    return num;
 }
+
+function updateDisplay() {
+    DISPLAY.textContent = displayText;
+}
+
+function calculate() {
+    if (operator === "div" && parseFloat(displayText) == 0){
+        displayText = "ERROR";
+        displayShowsError = true;
+        updateDisplay();
+        return;
+    }
+    firstNumber = (Math.round((operate(firstNumber, parseFloat(displayText), operator)) * 10000) / 10000);
+    displayText = firstNumber;
+    displayShowsResult = true;
+}
+
+function storeNumber(op) {
+    hasDecimal = false;
+    if (firstNumber === null) {
+        firstNumber = parseFloat(displayText);
+        displayText = "0";
+    }else if (operator === "") {
+        operator = op;
+    }else {
+        calculate();
+    }
+    updateDisplay();
+}
+
+function resetCalculator() {
+    operator = "";
+    firstNumber = null;
+    displayText = "0";
+    hasDecimal = false;
+    displayShowsResult = false;
+    displayShowsError = false;
+    lastButtonWasOperator = false;
+    updateDisplay();
+}
+
+function removeLastChar() {
+    if (displayShowsResult) {
+        return;
+    }
+    if (displayText.slice(displayText.length-1) === ".") {
+        hasDecimal = false;
+    }
+    displayText = displayText.slice(0, displayText.length-1);
+    if (displayText === "" || displayText === "-") {
+        displayText = "0";
+    }
+}
+
+function handleNumberButton(id) {
+    lastButtonWasOperator = false;
+    if (operator === "" && displayShowsResult) {
+        resetCalculator();
+    }
+    if (displayShowsResult) {
+        displayText = "0";
+        displayShowsResult = false;
+    }
+    switch (id) {
+        case "decimal": // Add decimal point
+            if (hasDecimal) {
+                break;
+            }
+            displayText += ".";
+            hasDecimal = true;
+            break;
+        case "sign": // Toggle sign
+            if (displayText.charAt(0) == "-") {
+                displayText = displayText.slice(1);
+            }else {
+                displayText = "-" + displayText;
+            }
+            break;
+        default: // Add clicked number to display
+            if (displayText === "-0") {
+                displayText = "-" + id;
+            }else if (displayText === "0") {
+                displayText = id;
+            }else {
+                displayText += id;
+            }
+             break;
+    }
+    updateDisplay();
+}
+
+function handleOperatorButton(id) {
+    switch (id) {
+        case "equals":
+            if (firstNumber === null) {
+                break;
+            }
+            calculate();
+            operator = "";
+            break;
+        case "clear":
+            resetCalculator();
+            break;
+        case "back":
+            removeLastChar();
+            break;
+        default: // Handle +-/*% buttons
+            if (!lastButtonWasOperator) {
+                storeNumber(id);
+            }
+            lastButtonWasOperator = true;
+            operator = id;
+            break;
+    }
+    updateDisplay();
+}
+
+function getClickedButton(target) {
+    if (target.classList.contains("operator")) {
+        if (displayShowsError) {
+            resetCalculator();
+        }
+        handleOperatorButton(target.id);
+    }else if (target.classList.contains("number")) {
+        if (displayShowsError) {
+            resetCalculator();
+        }
+        handleNumberButton(target.id);
+    }
+}
+
+BUTTONS.addEventListener("click", event => getClickedButton(event.target));
